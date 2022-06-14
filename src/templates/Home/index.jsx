89b8734ef627bd1ab9c4b822d7b1d49dfc9c153c
Lucian Binner <lucian.binner@hotmail.com>
2022-06-14 20:18:00 -0300

    feat: adding CounterContextProvider

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,4 +1,8 @@
+import { useCounterContext } from '../../contexts/CounterContext';
+
 const Home = () => {
+  const { state, dispatch } = useCounterContext();
+
   return (
     <div>
       <p>Ola</p>
