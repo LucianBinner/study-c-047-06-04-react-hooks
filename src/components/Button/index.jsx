497bed86131c9aa19bb28cab536f21be417dd2a3
Lucian Binner <lucian.binner@hotmail.com>
2022-06-14 21:15:28 -0300

    feat: inser actions in componentes

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,21 @@
 import { useCounterContext } from '../../contexts/CounterContext';
 
-const Button = ({ nameButton, nameAction }) => {
-  const { actions } = useCounterContext();
+const Button = ({ nameButton, nameAction, payload, disableLoading }) => {
+  const { state, actions } = useCounterContext();
+
+  const handleActions = () => {
+    actions[`${nameAction}`](payload)
+      ?.then((r) => console.log(r))
+      ?.catch((e) => console.log(e.name, ':', e.message));
+  };
+
   return (
-    <button onClick={() => actions[`${nameAction}`]()}>{nameButton}</button>
+    <button
+      onClick={() => handleActions()}
+      disabled={disableLoading && state.loading}
+    >
+      {nameButton}
+    </button>
   );
 };
 
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -6,6 +6,28 @@ const Home = () => {
     <div>
       <Heading />
       <Button nameAction={'increase'} nameButton={'Increase'} />
+      <Button nameAction={'decrease'} nameButton={'Decrease'} />
+      <Button nameAction={'reset'} nameButton={'Reset'} />
+      <Button
+        nameAction={'setCounter'}
+        nameButton={'SetCounter 10'}
+        payload={{ counter: 10 }}
+      />
+      <Button
+        nameAction={'setCounter'}
+        nameButton={'SetCounter 100'}
+        payload={{ counter: 100 }}
+      />
+      <Button
+        disableLoading={true}
+        nameAction={'asyncIncrease'}
+        nameButton={'AsyncIncrease'}
+      />
+      <Button
+        disableLoading={true}
+        nameAction={'asyncError'}
+        nameButton={'AsyncError'}
+      />
     </div>
   );
 };
